cb7966e8877963ea30e7635d91005df34d7ea42b
Robert Karish <robert@Roberts-MacBook-Pro.local>
2020-03-11 17:56:03 -0400

    added comments to the person test class

--- tests/AddressBook/PersonTest.java
+++ tests/AddressBook/PersonTest.java
@@ -11,6 +11,7 @@ public class PersonTest {
 
     @BeforeEach
     void setUp() {
+        // Make a new Person before each test case
         p = new Person("Robert", "Karish", "12345 Bonita Landing Circle", "Bonita Springs", "FL", "12345", "239-821-1466");
 
     }
@@ -22,59 +23,69 @@ public class PersonTest {
 
     @Test
     void getFirstNameTest() {
+        // Test the getFirstName method
         String firstName = "Robert";
         assertEquals(p.getFirstName(), firstName);
     }
 
     @Test
     void getLastNameTest() {
+        // Test the getLastName method
         String lastName = "Karish";
         assertEquals(p.getLastName(), lastName);
     }
 
     @Test
     void getAddressTest() {
+        // Test the getAddress method
         String address = "12345 Bonita Landing Circle";
         assertEquals(address,p.getAddress());
     }
 
     @Test
     void getCityTest() {
+        // Test the getCity method
         String city = "Bonita Springs";
         assertEquals(city, p.getCity());
     }
 
     @Test
     void getStateTest() {
+        // Test the getState method
         String state = "FL";
         assertEquals(state, p.getState());
     }
 
     @Test
     void getZipTest() {
+        // Test the getZip method
         String zip = "12345";
         assertEquals(zip, p.getZip());
     }
 
     @Test
     void getPhoneTest() {
+        // Test the getPhone method
         String phone = "239-821-1466";
         assertEquals(phone, p.getPhone());
     }
 
     @Test
     void toStringTest() {
+        // Test the toString method
         String toString = "Karish, Robert";
         assertEquals(toString, p.toString());
     }
 
     @Test
     void containsStringTest() {
+        // Test the containsString method
         assertEquals(true, p.containsString("Robert"));
     }
 
     @Test
     void getFieldTest() {
+        // Test the getField method
         String name = "Robert";
         assertEquals(name, p.getField(1));
     }
